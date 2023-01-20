d827a0fc37858e63f8d02c2b83cc9f42d98741ea
Martin Erzberger <martin@erzberger.ch>
2023-01-20 17:00:35 +0100

    Server done

--- server/src/main/java/ch/erzberger/hello/HelloEndpoint.java
+++ server/src/main/java/ch/erzberger/hello/HelloEndpoint.java
@@ -1,32 +1,36 @@
 package ch.erzberger.hello;
 
-import ch.erzberger.hello.jaxb.HelloRequest;
-import ch.erzberger.hello.jaxb.ObjectFactory;
-import jakarta.xml.bind.JAXBElement;
+import ch.erzi.home.soap.services.HelloPort;
+import ch.erzi.home.soap.types.hello.HelloRequest;
+import ch.erzi.home.soap.types.ping_1_0.PingRequest;
+import ch.erzi.home.soap.types.ping_1_0.PingResponse;
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
-import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
-import org.springframework.ws.server.endpoint.annotation.RequestPayload;
-import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 /**
  * The endpoint defines the operations of the service.
  */
 @Endpoint
-public class HelloEndpoint {
-    // This variable contains the namespace of the XML. While it looks like a URL, it is just a convention to do this.
-    // The service would still work on any machine with any URL as its server address.
-    // On the other hand, tools like SoapUI use the namespace to construct a default endpoint address.
-    // This is why it's a good idea to use the real URL of the server and not just some random namespace root.
-    private static final String NAMESPACE_URI = "https://soap.home.erzi.ch/services";
+public class HelloEndpoint implements HelloPort {
+    @Override
+    public PingResponse ping10(PingRequest parameters) {
+        PingResponse response = new PingResponse();
+        try {
+            InetAddress ip = InetAddress.getLocalHost();
+            response.setHostAddress(ip.getHostAddress());
+            response.setHostName(ip.getHostName());
+            return response;
+        } catch (UnknownHostException e) {
+            response.setHostName("localhost");
+            response.setHostAddress("127.0.0.1");
+            return response;
+        }
+    }
 
-    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "helloRequest")
-    @ResponsePayload
-    // One might be tempted to use a simple String as the response type. If this is done, the method is not found
-    // by the mapper. The XML response on the client will contain a fault similar to
-    // "No adapter for endpoint [public java.lang.String ch.erzberger.hello.HelloEndpoint.hello"
-    // So: Use the ObjectFactory to construct the reply, and set the reply type accordingly.
-    public JAXBElement<String> hello(@RequestPayload HelloRequest request) {
-        ObjectFactory factory = new ObjectFactory();
-        return factory.createHelloResponse(String.format("Hello %s %s", request.getFirstName(), request.getLastName()));
+    @Override
+    public String hello(HelloRequest helloRequest) {
+        return String.format("Hello %s %s", helloRequest.getFirstName(), helloRequest.getLastName());
     }
 }
